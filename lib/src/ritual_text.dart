35baaa5bfdae2303ee8675047e895fb6cc49f0ba
Thamizharasu S <zarub2k@gmail.com>
2018-03-09 19:51:29 +0530

    style part is added

--- lib/src/ritual_text.dart
+++ lib/src/ritual_text.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 class RitualsText extends StatelessWidget {
-  RitualsText(this.text);
+  RitualsText(this.text, {
+    Key key,
+    this.size
+  }) : super(key: key);
+
   final String text;
+  final RitualsTextSize size;
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +15,35 @@ class RitualsText extends StatelessWidget {
   }
 
   Text _buildUi() {
-    return new Text(text);
+    return new Text(text,
+      style: _style(),
+    );
+  }
+
+  TextStyle _style() {
+    return new TextStyle(
+      fontSize: _size()
+    );
+  }
+
+  double _size() {
+    if (RitualsTextSize.small == size) {
+      return 15.0;
+    }
+
+    if (RitualsTextSize.medium == size) {
+      return 20.0;
+    }
+
+    if (RitualsTextSize.large == size) {
+      return 25.0;
+    }
+
+    if (RitualsTextSize.xlarge == size) {
+      return 30.0;
+    }
+
+    return 10.0;
   }
 }
 
