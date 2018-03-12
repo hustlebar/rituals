d604aac9b6d7c9fc1e3ef16f16f09879e2278cc4
Thamizharasu S <zarub2k@gmail.com>
2018-03-12 18:36:36 +0530

    Icon module exported

--- lib/rituals.dart
+++ lib/rituals.dart
@@ -2,6 +2,7 @@ library rituals;
 
 export 'package:rituals/src/linear/rituals_text.dart';
 export 'package:rituals/src/linear/rituals_button.dart';
+export 'package:rituals/src/linear/rituals_icon.dart';
 /// A Calculator.
 //class Calculator {
 //  /// Returns [value] plus 1.
